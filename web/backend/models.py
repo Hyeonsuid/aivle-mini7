1f670380f95e493526fe298c3db7e5715595b240
Hyeonsu <khsoomail@gmail.com>
2022-11-22 14:18:19 +0900

    set admin

--- web/backend/models.py
+++ web/backend/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField('모델명', max_length=50)
+  description = models.CharField('설명', max_length=100, blank=True, help_text='설명을 적어주세요.')
+  upload = models.FileField(max_length=254, blank=True)
+  create_dt = models.DateTimeField('생성시간', auto_now_add=True)
+  update_dt = models.DateTimeField('수정시간', auto_now=True)
+
+  class Meta:
+    ordering = ('update_dt', )
+
+  def __str__(self):
+    return self.title
+  
+
---
+++ web/backend/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from backend import views
+
+app_name = 'backend'
+urlpatterns = [
+  
+]
