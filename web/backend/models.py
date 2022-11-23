ba7435ec1f7143b2f387d251e4342e35b99b2e5e
Hyeonsu <khsoomail@gmail.com>
2022-11-23 09:44:27 +0900

    update settings

--- web/backend/models.py
+++ web/backend/models.py
@@ -5,6 +5,7 @@ import os, uuid
 
 # Create your models here.
 class Post(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   title = models.CharField('모델명', max_length=50)
   description = models.CharField('설명', max_length=100, blank=True, help_text='설명을 적어주세요.')
   upload = models.FileField(max_length=254, blank=True)
