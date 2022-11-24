f7edd2a94227b3b052499894548246332593a826
Hyeonsu <khsoomail@gmail.com>
2022-11-24 14:02:28 +0900

    update backend.views

--- web/backend/views.py
+++ web/backend/views.py
@@ -1,38 +1,38 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-import logging
-from .models import AImodel
-logger = logging.getLogger('mylogger')
-# Create your views here.
+# from django.shortcuts import render
+# from django.shortcuts import redirect
+# import logging
+# from .models import AImodel
+# logger = logging.getLogger('mylogger')
+# # Create your views here.
 
-from .models import Post
-
-
-def index(request):
-    #서비스 처리 
-    models = AImodel.objects.all()
-    return render(request, 'model_input.html',{'models':models})
+# from .models import Post
 
 
+# def index(request):
+#     #서비스 처리 
+#     models = AImodel.objects.all()
+#     return render(request, 'model_input.html',{'models':models})
 
-def upload(request):
-    #서비스 처리 
-    if request.method == 'POST' and request.FILES['files']:
 
-        #form에서 전송한 파일을 획득한다.
 
-        file = request.FILES['files']
+# def upload(request):
+#     #서비스 처리 
+#     if request.method == 'POST' and request.FILES['files']:
+
+#         #form에서 전송한 파일을 획득한다.
+
+#         file = request.FILES['files']
               
-        aimodel = AImodel(ai_file=file, ai_version=request.POST['num'])
+#         aimodel = AImodel(ai_file=file, ai_version=request.POST['num'])
         
-        aimodel.save(aimodel)
+#         aimodel.save(aimodel)
         
         
         
    
-    # http method의 GET은 처리하지 않는다. 사이트 테스트용으로 남겨둠
-    else:
-        test = request.GET['test']
-        logger.error(('Something went wrong!!',test))
+#     # http method의 GET은 처리하지 않는다. 사이트 테스트용으로 남겨둠
+#     else:
+#         test = request.GET['test']
+#         logger.error(('Something went wrong!!',test))
 
-    return redirect('backend:index')
+#     return redirect('backend:index')
