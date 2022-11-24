8143c8499c7becc4472b87b1fc526379e727e84a
Hyeonsu <khsoomail@gmail.com>
2022-11-24 14:01:42 +0900

    update frontend.models

--- web/frontend/models.py
+++ web/frontend/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
+import uuid
+from backend.models import Post
 # Create your models here.
+
 class Result(models.Model):
-    image = models.ImageField(blank=True)
-    answer = models.CharField(max_length=10)
-    result = models.CharField(max_length=10)
-    is_answer_check = models.BooleanField(max_length=10)
-    pub_date = models.DateTimeField('date published')
+  id = models.BigAutoField(primary_key=True)
+  uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+  name = models.CharField(max_length=50)
+  version = models.CharField(max_length=50)
+  image = models.ImageField(blank=True)
+  answer = models.CharField(max_length=10)
+  result = models.CharField(max_length=10)
+  is_answer_check = models.BooleanField(max_length=10)
+  pub_date = models.DateTimeField('date published')
